61dff7ebfeedc530f98c3403c44e8ba53defc341
Chris83 <83odie@gmail.com>
2022-02-08 19:39:38 -0500

    Added a working UI

--- js/main.js
+++ js/main.js
@@ -1,57 +1,78 @@
 const shoot = ['Rock', 'Paper', 'Scissors'];
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+const game = document.querySelector('#game');
+
 function computerPlay() {
    return shoot[Math.floor(Math.random()*3)];
 }
 
-function playerPlay() {
-   playerSelection = prompt("Are you shooting Rock, Paper, or Scissors?", "");
+function playRound() {
+   playerSelection = this.value;
+   computerSelection = computerPlay();
 
-   // Format the players input to equal "Rock", "Paper", or "Scissors"
-   return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-}
+  if(game.childElementCount >= 7) startNewGame();
 
-function playRound(playerSelection, computerSelection) {
-   if( computerSelection == playerSelection) {
-      return `You tied! ${playerSelection} ties with ${computerSelection}`;
-   } else if (playerSelection == "Rock" && computerSelection == "Scissors" 
-               || playerSelection == "Paper" && computerSelection == "Rock" 
-               || playerSelection == "Scissors" && computerSelection == "Paper") {
-      return `You win! ${playerSelection} beats ${computerSelection}`;
+   let results = ''
+   if( computerSelection === playerSelection) {
+      results = `You tied that round! ${playerSelection} ties with ${computerSelection}`;
+   } else if (playerSelection === "Rock" && computerSelection === "Scissors" 
+               || playerSelection === "Paper" && computerSelection === "Rock" 
+               || playerSelection === "Scissors" && computerSelection === "Paper") {
+       results = `You won that round! ${playerSelection} beats ${computerSelection}`;
    } else {
-      return `You lost! ${playerSelection} loses to ${computerSelection}`;
+      results = `You lost that round! ${playerSelection} loses to ${computerSelection}`;
    }
+
+   calculateScore(results);
+
+   const pResult = document.createElement('p');
+   pResult.textContent = results;
+   game.appendChild(pResult);
 }
 
-function game() {
-   const rounds = 5;
-   let score = {wins: 0, losses: 0, ties: 0};
-
-   for(let i=0; i<rounds; i++) {
-      let result = playRound(playerPlay(), computerPlay());
-      score =  calculateScore(result, score);
-      console.log(result);
-   }
-   
-   console.log(`Your Wins: ${score.wins} - Losses: ${score.losses} - Ties: ${score.ties}`);
-
-   if(confirm("Would you like to play again?")) game();
-}
-
-function calculateScore(result, score){
-   const regExWin = /win/;
-   const regExLoss = /lose/;
+function calculateScore(result){
+   const regExWin = /won/;
+   const regExLoss = /lost/;
 
    if(regExWin.test(result)) {
-      score.wins++;
-      return score;
+      updateScore('wins');
    } else if(regExLoss.test(result)) {
-      score.losses++;
-      return score;
+      updateScore('losses');
    } else {
-      score.ties++;
-      return score;
+      updateScore('ties');
    }
+}
 
-   return score;
+function updateScore(result){
+   const score = document.querySelector(`.${result}`);
+   score.textContent = Number.parseInt(score.textContent) + 1;
+
+   const gameCount = document.querySelector('.winner');
+   gameCount.dataset.gamecount = Number.parseInt(gameCount.dataset.gamecount) + 1;
+   if(gameCount.dataset.gamecount == 5) {
+      announceWinner();
+   }
+}
+
+function announceWinner() {
+   const wins = Number.parseInt(document.querySelector('.wins').textContent);
+   const losses = Number.parseInt(document.querySelector('.losses').textContent);
+
+   const pWinner = document.querySelector('.winner');
+
+   if(wins > losses){
+      pWinner.textContent = "YOU ARE THE CHAMPION!!!";
+   } else if (losses > wins) {
+      pWinner.textContent = "You lost, better luck next time."
+   } else {
+      pWinner.textContent = "You both win? or lost... Looks like a tie to me."
+   }
+}
+
+function startNewGame() {
+   game.innerHTML = '<p class="winner" data-gamecount="0"></p>' +
+   '<p>Wins: <span class="wins">0</span> Losses: <span class="losses">0</span> Ties: <span class="ties">0</span></p>';
 }
